db43a3ee5432b9d0499fdee215c267bad8b76237
Ashton Putnam <112705371+ashton-put@users.noreply.github.com>
2023-02-08 00:52:46 -0500

    Add files via upload

--- NumberTester.java
+++ NumberTester.java
@@ -11,139 +11,113 @@
 
 import java.util.Scanner; //Make a scanner for keyboard input
 import java.io.*; //Import IO for file input and output
+
 public class NumberTester
 {
    public static void main(String [] args) throws Exception
    {
-      int n;
-      int q;
+      Scanner infile;
+      infile = new Scanner(new File("numbers.txt"));
       
-      String outputName;
+      PrintWriter outfile;
+      outfile = new PrintWriter("numberSummary.txt");
       
-      Scanner keyboard = new Scanner(System.in);
+      String s = ("Repeat");
+      String b = ("Number");
+      String m = ("Digits");
+      String o = ("Even");
+      String g = ("Prime");
+         
+      outfile.printf("%15s\n", s);
+      outfile.printf("%7s %7s %7s %7s\n", b, m ,o, g);
       
-      System.out.print("Name of the input file? (Probably numbers.txt): ");
-      String inputName = keyboard.nextLine();
-      File inputFile = new File(inputName);
-      Scanner inputReader = new Scanner(inputFile);
-      
-      while (inputReader.hasNext())
+      while (infile.hasNext())
       {
-         n = inputReader.nextInt();
-         System.out.println(n);
-      }
+         int n = infile.nextInt();
+         String r1;
+         String r2;
+         String r3;
                   
-      /**
-         isPrime takes an integer value (n) and returns true if
-         the number is prime, false otherwise.
-         @param n integer to be tested to determine if prime.
-         @return boolean value if n is prime.
-      */
-
-   public static boolean isPrime(int n)
-   {
-      boolean status;
-         
-      if (n > 0 && n % 2 == 0)
-         status = true;
-      else
-         status = false;
-            
-      return status;
-   }
-      
-      
-      /**
-         isEven takes an integer value (n) and returns true if
-         the number is even, false otherwise.
-         @param n integer to be tested to determine if even.
-         @return boolean value if n is even.
-      */
-      /*
-      public static boolean isEven(int n)
-      {
-         if (n > 0 && n % 2 == 0)
-            return true;
+         if(isRepeatedDigits(n))
+            r1 = "+";
          else
-            return false;
-      }
-      */
-      
-      /**
-         isRepeatedDigits takes an integer value (n) and returns true if
-         the number contains the same digit is adjacent
-         locations in the number, false otherwise.
-         @param n integer to be tested to determine repeated values.
-         @return boolean value if n contains repeated values.
-      */
-      /*
-      public static boolean isRepeatedDigits(int n)
-      {
-         String s = " " + num 
-         Integer.toString()
+            r1 = "-";
          
-         for (i=0; i<s.length()-1; i++)
-            s.charAt(i)
+         if(isEven(n))
+            r2 = "+";
+         else
+            r2 = "-";
+         
+         if(isPrime(n))
+            r3 = "+";
+         else
+            r3 = "-";
+         
+         outfile.printf("%5d %6s %8s %7s\n", n, r1, r2, r3); 
       }
-      */
+      
+      infile.close();
+      outfile.close();
+   }
+   
+                  
+   /**
+      isPrime takes an integer value (n) and returns true if
+      the number is prime, false otherwise.
+      @param n integer to be tested to determine if prime.
+      @return boolean value if n is prime.
+   */
+   public static boolean isPrime(int n)
+   {  
+      for (int i = 2; i <= n - 1; i++)
+      {
+         if ((n % i) == 0)
+         {
+            return false;
+         }
+      }
+      return true;  
+   }  
+     
+      
+   /**
+      isEven takes an integer value (n) and returns true if
+      the number is even, false otherwise.
+      @param n integer to be tested to determine if even.
+      @return boolean value if n is even.
+   */
+   public static boolean isEven(int n)
+   {
+      if (n % 2 == 0)
+      {
+         return true;
+      }
+      return false;
+   }
+
+   
+   /**
+      isRepeatedDigits takes an integer value (n) and returns true if
+      the number contains the same digit is adjacent
+      locations in the number, false otherwise.
+      @param n integer to be tested to determine repeated values.
+      @return boolean value if n contains repeated values.
+   */
+   public static boolean isRepeatedDigits(int n)
+   {
+      String s = "" + n;
+      
+      int i;
+      
+      for (i=0; i < s.length()-1; i++)
+      {
+         
+         
+         if(s.charAt(i) == s.charAt(i+1))
+         {
+            return true;
+         }
+      }
+   return false;
    }
 }
-
-/*
-
-prime: n/2 
-output prints two seperate lines
-then prints the formatted ouput data
-
-577 - has repeating
-575 - does not have repeating
-
-take the number, convert to string, 
-each single digit a character,
-
-String s = "" + num 
-Integer.toString()
-
-s.charAt(i) - finds what character is at location
-
-loop through characters, stop if you find that the character at i
-and the character at i+1 is the same, or get to the end of the string
-
-when looping through values, 
-
-for (i=0; i<s.length()-1; i++)
-   s.charAt(i)
-
-//FORMATTING HEADER FOR OUTPUT:
-//figure out spacing
-
-   1[        Repeat
-   2[Number Digit Prime Even
-   3[477 +  -  -
-   4[ %7s - MAGIC KEY TO SPACING
-   
-use an if statement in main to add + or -, methods
-only return a boolean value
-
-if(isPrime(n))
-   result = "+"
-else
-   result = "-"
-   
-//PrintWriter REVIEW
-
-attach to a file
-
-PrintWriter outfile = new PrintWriter("results.txt")
-{
-   exactly the same use as System.out.
-   
-   outfile.print();
-   outfile.println();
-   outfile.printf();
-}
-outfile.close();
-   
-
-
-*/
