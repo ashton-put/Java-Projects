5bf788d69e5c1f8020317856540e7b3fee78c7d3
Ashton Putnam <112705371+ashton-put@users.noreply.github.com>
2023-02-08 21:57:26 -0500

    initial

---
+++ SavingsAccount.java
@@ -0,0 +1,36 @@
+public class SavingsAccount
+{
+   private double balance;
+   private double interestRate;
+   private double lastInterest;
+   
+   public SavingsAccount(double x, double y);
+   {
+      balance = x;
+      interestRate = y;
+   }
+   
+   public void withdraw(double w)
+   {
+   }
+   
+   public void deposit(double d)
+   {
+   }
+   
+   public void addInterest()
+   {
+   }
+   
+   public double getBalance()
+   {
+   }
+   
+   public double getInterestRate()
+   {
+   }
+   
+   public double getLastInterest()
+   {
+   }
+}
